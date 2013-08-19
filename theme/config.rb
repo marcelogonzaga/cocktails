59147f348ff805ef752796ac4e2b554013ed2b02
LucasAzevedoNet <design.lucas@gmail.com>
2013-08-19 14:56:46 -0300

    add

--- theme/config.rb
+++ theme/config.rb
@@ -1,8 +1,8 @@
 ﻿dir = File.dirname(__FILE__)
 load File.join(dir, '..', 'cocktails')
 sass_path = dir
-css_path = File.join(dir,'..', 'css')
-images_dir = '../images' 
+css_path = File.join(dir,'..','..', 'css')
+images_dir = '../../images' 
 output_style = :expanded
 line_comments = false
 relative_assets = true
